ba3a1c9219b34ab1ff58485aa9935a135b64f82c
kungurtsev <kungurtsev@skbkontur.ru>
2019-09-03 14:07:19 +0500

    dep vostok.hosting.abstractions@vostok-application

---
+++ Vostok.Hosting/VostokHostingEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Vostok.Hosting.Abstractions;
+
+namespace Vostok.Hosting
+{
+    internal class VostokApplication : IVostokApplication
+    {
+        public Task InitializeAsync(IVostokHostingEnvironment environment) =>
+            throw new System.NotImplementedException();
+
+        public Task RunAsync(IVostokHostingEnvironment environment) =>
+            throw new System.NotImplementedException();
+    }
+}
