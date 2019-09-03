bcf3d39d606724a8f902ad631697d7ec2718c7ad
kungurtsev <kungurtsev@skbkontur.ru>
2019-09-03 14:11:11 +0500

    back VostokHostingEnvironment

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/github/vostok/hosting?svg=true&branch=master)](https://ci.appveyor.com/project/vostok/hosting/branch/master)
 [![NuGet](https://img.shields.io/nuget/v/Vostok.Hosting.svg)](https://www.nuget.org/packages/Vostok.Hosting)
 
-The implementation of VostokApplication - the entry point to create Vostok-compatible services.
+The entry point to create Vostok-compatible services.
 
 
 **Build guide**: https://github.com/vostok/devtools/blob/master/library-dev-conventions/how-to-build-a-library.md
--- Vostok.Hosting/VostokHostingEnvironment.cs
+++ Vostok.Hosting/VostokHostingEnvironment.cs
@@ -1,14 +1,31 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using Vostok.Clusterclient.Core;
+using Vostok.Configuration.Abstractions;
+using Vostok.Context;
+using Vostok.Hercules.Client.Abstractions;
 using Vostok.Hosting.Abstractions;
+using Vostok.Logging.Abstractions;
+using Vostok.ServiceDiscovery.Abstractions;
+using Vostok.Tracing.Abstractions;
 
 namespace Vostok.Hosting
 {
-    internal class VostokApplication : IVostokApplication
+    internal class VostokHostingEnvironment : IVostokHostingEnvironment
     {
-        public Task InitializeAsync(IVostokHostingEnvironment environment) =>
-            throw new System.NotImplementedException();
-
-        public Task RunAsync(IVostokHostingEnvironment environment) =>
-            throw new System.NotImplementedException();
+        public CancellationToken ShutdownToken { get; }
+        public IVostokApplicationIdentity ApplicationIdentity { get; }
+        public IVostokApplicationMetrics Metrics { get; }
+        public ILog Log { get; }
+        public ITracer Tracer { get; }
+        public IHerculesSink HerculesSink { get; }
+        public IConfigurationSource ConfigurationSource { get; }
+        public IConfigurationProvider ConfigurationProvider { get; }
+        public IServiceBeacon ServiceBeacon { get; }
+        public IServiceLocator ServiceLocator { get; }
+        public IContextGlobals ContextGlobals { get; }
+        public IContextProperties ContextProperties { get; }
+        public IContextConfiguration ContextConfiguration { get; }
+        public ClusterClientSetup ClusterClientSetup { get; }
+        public IVostokHostExtensions HostExtensions { get; }
     }
 }
