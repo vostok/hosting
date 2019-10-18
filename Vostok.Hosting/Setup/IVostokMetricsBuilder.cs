e299a995b649157e00cb96e3d7877f9bc9a0914a
kungurtsev <kungurtsev@skbkontur.ru>
2019-10-18 12:56:33 +0500

    VostokHostingEnvironmentSetupExtensions

--- Vostok.Hosting/Setup/IVostokMetricsBuilder.cs
+++ Vostok.Hosting/Setup/IVostokMetricsBuilder.cs
@@ -7,7 +7,6 @@ namespace Vostok.Hosting.Setup
     [PublicAPI]
     public interface IVostokMetricsBuilder
     {
-        IVostokMetricsBuilder SetupHerculesMetricEventSender();
         IVostokMetricsBuilder SetupHerculesMetricEventSender([NotNull] Action<IVostokHerculesMetricEventSenderBuilder> herculesMetricEventSenderSetup);
 
         IVostokMetricsBuilder AddMetricEventSender([NotNull] IMetricEventSender metricEventSender);
---
+++ Vostok.Hosting/Setup/VostokHostingEnvironmentSetupExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Setup
+{
+    [PublicAPI]
+    public static class VostokHostingEnvironmentSetupExtensions
+    {
+        public static IVostokCompositeLogBuilder SetupFileLog(this IVostokCompositeLogBuilder builder) =>
+            builder.SetupFileLog(_ => {});
+
+        public static IVostokCompositeLogBuilder SetupConsoleLog(this IVostokCompositeLogBuilder builder) =>
+            builder.SetupConsoleLog(_ => {});
+
+        public static IVostokHostingEnvironmentBuilder SetupServiceBeacon(this IVostokHostingEnvironmentBuilder builder) =>
+            builder.SetupServiceBeacon(_ => {});
+
+        public static IVostokMetricsBuilder SetupHerculesMetricEventSender(this IVostokMetricsBuilder builder) =>
+            builder.SetupHerculesMetricEventSender(_ => {});
+    }
+}
