9480ac9d90f01ae5bfc90d7b1c11f64d50b5f55a
kungurtsev <kungurtsev@skbkontur.ru>
2019-10-28 17:30:53 +0500

    Added a basic sanity-check unit test that runs a hello world application in a host and ensures that nothing crashes.

---
+++ Vostok.Hosting.Tests/VostokHost_Tests.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Hosting.Abstractions;
+using Vostok.Hosting.Models;
+using Vostok.Hosting.Setup;
+using Vostok.Logging.Abstractions;
+
+namespace Vostok.Hosting.Tests
+{
+    [TestFixture]
+    internal class VostokHost_Tests
+    {
+        [Test]
+        public void Should_run_simple_application()
+        {
+            var application = new SimpleApplication();
+
+            VostokHostingEnvironmentSetup environmentSetup = setup =>
+            {
+                setup
+                    .SetupApplicationIdentity(
+                        (applicationIdentitySetup, setupContext) => applicationIdentitySetup
+                            .SetProject("Infrastructure")
+                            .SetSubproject("vostok")
+                            .SetEnvironment(setupContext.ClusterConfigClient.Get("app/environment")?.Value ?? "default")
+                            .SetApplication("simple-application")
+                            .SetInstance("1")
+                    )
+                    .SetupLog(
+                        logSetup =>
+                            logSetup
+                                .SetupConsoleLog(
+                                    consoleLogSetup =>
+                                        consoleLogSetup.UseSynchronous()));
+            };
+
+            var host = new VostokHost(new VostokHostSettings(application, environmentSetup));
+
+            var result = host.RunAsync().GetAwaiter().GetResult();
+
+            result.Status.Should().Be(VostokApplicationRunStatus.Exited);
+
+            application.Initialized.Should().BeTrue();
+            application.Run.Should().BeTrue();
+        }
+
+        private class SimpleApplication : IVostokApplication
+        {
+            public bool Initialized;
+            public bool Run;
+
+            public Task InitializeAsync(IVostokHostingEnvironment environment)
+            {
+                Initialized = true;
+
+                environment.Log.Info("Hello from app!");
+
+                return Task.CompletedTask;
+            }
+
+            public Task RunAsync(IVostokHostingEnvironment environment)
+            {
+                Run = true;
+
+                environment.Log.Info("Hello again from app!");
+                
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
