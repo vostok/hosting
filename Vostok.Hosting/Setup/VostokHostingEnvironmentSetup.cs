337791ba38ec36a53adbd25b37abf07d48e78b5c
kungurtsev <kungurtsev@skbkontur.ru>
2019-10-30 12:44:38 +0500

    more docs

--- Vostok.Hosting/Setup/VostokHostingEnvironmentSetup.cs
+++ Vostok.Hosting/Setup/VostokHostingEnvironmentSetup.cs
@@ -1,7 +1,11 @@
 ﻿using JetBrains.Annotations;
+using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Hosting.Setup
 {
+    /// <summary>
+    /// Delegate which configures <see cref="IVostokHostingEnvironment"/>.
+    /// </summary>
     [PublicAPI]
     public delegate void VostokHostingEnvironmentSetup([NotNull] IVostokHostingEnvironmentBuilder builder);
 }
--- Vostok.Hosting/VostokHostSettings.cs
+++ Vostok.Hosting/VostokHostSettings.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using JetBrains.Annotations;
 using Vostok.Commons.Time;
+using Vostok.Hercules.Client.Abstractions;
 using Vostok.Hosting.Abstractions;
 using Vostok.Hosting.Setup;
+using Vostok.Logging.Abstractions;
+using Vostok.Tracing.Abstractions;
 
 namespace Vostok.Hosting
 {
+    /// <summary>
+    /// Represents configuration of <see cref="VostokHost"/>.
+    /// </summary>
     [PublicAPI]
     public class VostokHostSettings
     {
@@ -15,16 +21,33 @@ namespace Vostok.Hosting
             EnvironmentSetup = environmentSetup ?? throw new ArgumentNullException(nameof(environmentSetup));
         }
 
+        /// <summary>
+        /// An application which will be run.
+        /// </summary>
         [NotNull]
         public IVostokApplication Application { get; }
 
+        /// <summary>
+        /// A delegate which will be used to configure <see cref="IVostokHostingEnvironment"/>.
+        /// </summary>
         [NotNull]
         public VostokHostingEnvironmentSetup EnvironmentSetup { get; }
 
+        /// <summary>
+        /// <para>Whether or not to configure static providers before running application.</para>
+        /// <para>See <see cref="LogProvider"/>, <see cref="TracerProvider"/> and <see cref="HerculesSinkProvider"/> for more information.</para>
+        /// </summary>
         public bool ConfigureStaticProviders { get; set; } = true;
 
+        /// <summary>
+        /// <para>Whether or not to configure thread pool.</para>
+        /// <para>If set to <c>false</c>, should be configured manually before <see cref="VostokHost.RunAsync"/> will be called.</para>
+        /// </summary>
         public bool ConfigureThreadPool { get; set; } = true;
 
+        /// <summary>
+        /// Timeout for application graceful shutdown, after <see cref="IVostokHostingEnvironment.ShutdownToken"/> has been canceled.
+        /// </summary>
         public TimeSpan ShutdownTimeout { get; set; } = 5.Seconds();
     }
 }
