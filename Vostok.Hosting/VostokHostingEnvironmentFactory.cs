50cfdd8b1f61098fb7a27b71f7dca6309f6bc6b3
kungurtsev <kungurtsev@skbkontur.ru>
2019-11-18 16:37:26 +0500

    add doc

--- Vostok.Hosting/VostokHostingEnvironmentFactory.cs
+++ Vostok.Hosting/VostokHostingEnvironmentFactory.cs
@@ -13,6 +13,8 @@ namespace Vostok.Hosting
         /// <summary>
         /// Creates an instance of <see cref="IVostokHostingEnvironment"/>, using given <paramref name="setup"/> and <paramref name="shutdownToken"/>.
         /// </summary>
+        /// <param name="setup">Delegate which configures <see cref="IVostokHostingEnvironment"/>.</param>
+        /// <param name="shutdownToken">Cancellation token, that will be passed into <see cref="IVostokHostingEnvironment.ShutdownToken"/>.</param>
         [NotNull]
         public static IVostokHostingEnvironment Create([NotNull] VostokHostingEnvironmentSetup setup, CancellationToken shutdownToken = default)
             => EnvironmentBuilder.Build(setup ?? throw new ArgumentNullException(nameof(setup)), shutdownToken);
