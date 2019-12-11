47c7f5f683a312bfa53d6b9da9a15eeb0bd95e75
iloktionov <iloktionov@skbkontur.ru>
2019-12-11 16:00:08 +0500

    IVostokConfigurationContext: added SecretConfigurationSource, improved xml docs.

--- Vostok.Hosting/Setup/IVostokConfigurationContext.cs
+++ Vostok.Hosting/Setup/IVostokConfigurationContext.cs
@@ -1,21 +1,26 @@
 ﻿using JetBrains.Annotations;
 using Vostok.ClusterConfig.Client.Abstractions;
 using Vostok.Configuration.Abstractions;
+using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Hosting.Setup
 {
     [PublicAPI]
     public interface IVostokConfigurationContext
     {
-        /// <inheritdoc cref="IConfigurationSource"/>
+        /// <inheritdoc cref="IVostokHostingEnvironment.ConfigurationSource"/>
         [NotNull]
         IConfigurationSource ConfigurationSource { get; }
 
-        /// <inheritdoc cref="IConfigurationProvider"/>
+        /// <inheritdoc cref="IVostokHostingEnvironment.SecretConfigurationSource"/>
+        [NotNull]
+        IConfigurationSource SecretConfigurationSource { get; }
+
+        /// <inheritdoc cref="IVostokHostingEnvironment.ConfigurationProvider"/>
         [NotNull]
         IConfigurationProvider ConfigurationProvider { get; }
 
-        /// <inheritdoc cref="IClusterConfigClient"/>
+        /// <inheritdoc cref="IVostokHostingEnvironment.ClusterConfigClient"/>
         [NotNull]
         IClusterConfigClient ClusterConfigClient { get; }
     }
