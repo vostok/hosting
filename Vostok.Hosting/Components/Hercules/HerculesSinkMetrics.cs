7bd6898d3852024c6be101e9b4b7b5b74c3bb76e
iloktionov <iloktionov@skbkontur.ru>
2020-01-06 22:40:02 +0500

    Minor cleanup.

--- Vostok.Hosting/Components/Hercules/HerculesSinkMetrics.cs
+++ Vostok.Hosting/Components/Hercules/HerculesSinkMetrics.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
 using Vostok.Commons.Time;
 using Vostok.Hercules.Client;
 using Vostok.Hercules.Client.Abstractions;
