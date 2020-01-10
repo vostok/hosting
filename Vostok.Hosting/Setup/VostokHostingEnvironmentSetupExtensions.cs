17750fe2c1e7c4519360ae2086f7d3b3f8c960ec
iloktionov <iloktionov@skbkontur.ru>
2020-01-10 15:24:03 +0500

    Added SetBaseUrlPath extension.

--- Vostok.Hosting/Setup/VostokHostingEnvironmentSetupExtensions.cs
+++ Vostok.Hosting/Setup/VostokHostingEnvironmentSetupExtensions.cs
@@ -31,9 +31,15 @@ namespace Vostok.Hosting.Setup
             builder.SetupHerculesMetricEventSender(_ => {});
 
         /// <summary>
-        /// Sets given <paramref name="port"/> to <see cref="IServiceBeacon"/>.
+        /// Applies given <paramref name="port"/> to <see cref="IServiceBeacon"/> configuration.
         /// </summary>
         public static IVostokHostingEnvironmentBuilder SetPort([NotNull] this IVostokHostingEnvironmentBuilder builder, int port) =>
             builder.SetupServiceBeacon(serviceBeaconSetup => serviceBeaconSetup.SetupReplicaInfo(replicaInfoSetup => replicaInfoSetup.SetPort(port)));
+
+        /// <summary>
+        /// Applies given base url <paramref name="path"/> to <see cref="IServiceBeacon"/> configuration.
+        /// </summary>
+        public static IVostokHostingEnvironmentBuilder SetBaseUrlPath([NotNull] this IVostokHostingEnvironmentBuilder builder, [NotNull] string path) =>
+            builder.SetupServiceBeacon(serviceBeaconSetup => serviceBeaconSetup.SetupReplicaInfo(replicaInfoSetup => replicaInfoSetup.SetUrlPath(path)));
     }
 }
