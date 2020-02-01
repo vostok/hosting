7116f01cd7b76800612948748b450e91c545c0a5
iloktionov <iloktionov@skbkontur.ru>
2020-02-01 15:26:24 +0500

    Added configuration extensions for common sources.

---
+++ Vostok.Hosting/Setup/IVostokConfigurationBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Configuration.Sources.CommandLine;
+using Vostok.Configuration.Sources.Environment;
+using Vostok.Configuration.Sources.Json;
+using Vostok.Configuration.Sources.Object;
+using Vostok.Configuration.Sources.Yaml;
+
+namespace Vostok.Hosting.Setup
+{
+    [PublicAPI]
+    public static class IVostokConfigurationBuilderExtensions
+    {
+        public static IVostokConfigurationBuilder AddEnvironmentVariables([NotNull] this IVostokConfigurationBuilder builder)
+            => builder.AddSource(new EnvironmentVariablesSource());
+
+        public static IVostokConfigurationBuilder AddCommandLineArguments([NotNull] this IVostokConfigurationBuilder builder, [NotNull] string[] args)
+            => builder.AddSource(new CommandLineSource(args));
+
+        public static IVostokConfigurationBuilder AddCommandLineArguments([NotNull] this IVostokConfigurationBuilder builder, [NotNull] string[] args, [CanBeNull] string defaultKey, [CanBeNull] string defaultValue)
+            => builder.AddSource(new CommandLineSource(args, defaultKey, defaultValue));
+
+        public static IVostokConfigurationBuilder AddInMemoryObject([NotNull] this IVostokConfigurationBuilder builder, [NotNull] object @object)
+            => builder.AddSource(new ObjectSource(@object));
+
+        public static IVostokConfigurationBuilder AddJsonFile([NotNull] this IVostokConfigurationBuilder builder, [NotNull] string path)
+            => builder.AddSource(new JsonFileSource(path));
+
+        public static IVostokConfigurationBuilder AddSecretJsonFile([NotNull] this IVostokConfigurationBuilder builder, [NotNull] string path)
+            => builder.AddSecretSource(new JsonFileSource(path));
+
+        public static IVostokConfigurationBuilder AddAppSettingsJson([NotNull] this IVostokConfigurationBuilder builder)
+            => builder
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json");
+
+        public static IVostokConfigurationBuilder AddYamlFile([NotNull] this IVostokConfigurationBuilder builder, [NotNull] string path)
+            => builder.AddSource(new YamlFileSource(path));
+
+        public static IVostokConfigurationBuilder AddSecretYamlFile([NotNull] this IVostokConfigurationBuilder builder, [NotNull] string path)
+            => builder.AddSecretSource(new YamlFileSource(path));
+    }
+}
