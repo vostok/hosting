7f165b56cf16bc335c457195fd53897e3a39e73a
Цуп Илья Олегович <tsup@skbkontur.ru>
2020-08-28 10:58:48 +0500

    Added helpers for LevelCountingLog

---
+++ Vostok.Hosting/Helpers/ConcurrentCounter.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace Vostok.Hosting.Helpers
+{
+    internal class ConcurrentCounter
+    {
+        private volatile int value;
+
+        public void Increment() => Interlocked.Increment(ref value);
+
+        public int CollectAndReset() => Interlocked.Exchange(ref value, 0);
+    }
+}
