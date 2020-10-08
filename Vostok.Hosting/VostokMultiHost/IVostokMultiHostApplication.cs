427eb42426a1cc872990e7cc36c74df19ccfd977
Цуп Илья Олегович <tsup@skbkontur.ru>
2020-10-08 16:20:25 +0500

    Some templates

---
+++ Vostok.Hosting/VostokMultiHost/IVostokMultiHostApplication.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Vostok.Hosting.Models;
+
+namespace Vostok.Hosting.VostokMultiHost
+{
+    public interface IVostokMultiHostApplication
+    {
+        string Name { get; }
+
+        VostokApplicationState ApplicationState { get; }
+
+        Task<VostokApplicationRunResult> RunAsync();
+
+        Task StartAsync(VostokApplicationState? stateToAwait = VostokApplicationState.Running);
+
+        Task<VostokApplicationRunResult> StopAsync(bool ensureSuccess = true);
+    }
+}
