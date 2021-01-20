3f7c837e254622800a6352b41a7b1e304ee7c27a
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-01-20 10:14:19 +0500

    Added extension to IVostokHostingEnvironment

---
+++ Vostok.Hosting/Helpers/IVostokHostingEnvironmentExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Vostok.Commons.Threading;
+using Vostok.Hosting.Abstractions;
+
+namespace Vostok.Hosting.Helpers
+{
+    public static class IVostokHostingEnvironmentExtensions
+    {
+        public static void ConfigureThreadPool(this IVostokHostingEnvironment environment, int multiplier)
+        {
+            if (environment.ApplicationLimits.CpuUnits.HasValue)
+                ThreadPoolUtility.Setup(multiplier, environment.ApplicationLimits.CpuUnits.Value);
+            else
+                ThreadPoolUtility.Setup(multiplier);
+        }
+    }
+}
