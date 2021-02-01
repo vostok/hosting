248827e5a84631326d5378c9ae0bc299dd4c515a
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-02-01 11:14:45 +0500

    WIP: Added doc

--- Vostok.Hosting/Components/ThreadPool/ThreadPoolSettings.cs
+++ Vostok.Hosting/Components/ThreadPool/ThreadPoolSettings.cs
@@ -1,13 +1,23 @@
 ﻿using JetBrains.Annotations;
+using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Hosting.Components.ThreadPool
 {
-    // TODO: Write doc.
+    /// <summary>
+    /// Settings class that is used to dynamically configure thread pool.
+    /// </summary>
     [PublicAPI]
     public class ThreadPoolSettings
     {
+        /// <summary>
+        /// Per-core thread pool configuration multiplier.
+        /// </summary>
         public int ThreadPoolMultiplier { get; set; }
-        
+
+        /// <summary>
+        /// It is used for reconfiguration when on <see cref="IVostokApplicationLimits"/> change.
+        /// Actual value of this setting is <see cref="IVostokApplicationLimits.CpuUnits"/>.
+        /// </summary>
         internal float? CpuUnits { get; set; }
     }
 }
