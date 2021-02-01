4b53eb9de1200ebb81cfc1895f19617545f20c6e
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-02-01 11:04:05 +0500

    WIP: Filled skeleton with bodies

--- Vostok.Hosting/Components/ThreadPool/DynamicThreadPoolBuilder.cs
+++ Vostok.Hosting/Components/ThreadPool/DynamicThreadPoolBuilder.cs
@@ -1,13 +1,72 @@
 ﻿using System;
+using Vostok.Commons.Helpers;
+using Vostok.Configuration.Abstractions;
 using Vostok.Hosting.Setup;
 
 namespace Vostok.Hosting.Components.ThreadPool
 {
     internal class DynamicThreadPoolBuilder : IVostokDynamicThreadPoolBuilder, IBuilder<DynamicThreadPoolTracker>
     {
+        private readonly Customization<DynamicThreadPoolTrackerSettings> settingsCustomization;
+        private volatile bool enabled;
+
         public DynamicThreadPoolTracker Build(BuildContext context)
         {
-            throw new NotImplementedException();
+            if (!enabled)
+            {
+                context.LogDisabled("Dynamic thread pool");
+                return null;
+            }
+
+            var customizedSettings = settingsCustomization.Customize(new DynamicThreadPoolTrackerSettings());
+
+            return new DynamicThreadPoolTracker(
+                customizedSettings.ChecksPeriod,
+                context.ConfigurationProvider,
+                context.ApplicationLimits,
+                customizedSettings.ThreadPoolSettingsProvider,
+                context.Log);
+        }
+
+        public IVostokDynamicThreadPoolBuilder Enable()
+        {
+            enabled = true;
+
+            return this;
+        }
+
+        public IVostokDynamicThreadPoolBuilder Disable()
+        {
+            enabled = false;
+
+            return this;
+        }
+
+        public IVostokDynamicThreadPoolBuilder SetCheckPeriod(TimeSpan checkPeriod)
+        {
+            settingsCustomization.AddCustomization(settings => settings.ChecksPeriod = checkPeriod);
+            enabled = true;
+
+            return this;
+        }
+
+        public IVostokDynamicThreadPoolBuilder SetThreadPoolProvider(Func<IConfigurationProvider, ThreadPoolSettings> provider)
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+
+            settingsCustomization.AddCustomization(settings => settings.ThreadPoolSettingsProvider = provider);
+            enabled = true;
+
+            return this;
+        }
+
+        public IVostokDynamicThreadPoolBuilder CustomizeDynamicThreadPoolSettings(Action<DynamicThreadPoolTrackerSettings> customization)
+        {
+            settingsCustomization.AddCustomization(customization ?? throw new ArgumentNullException(nameof(customization)));
+            enabled = true;
+
+            return this;
         }
     }
 }
--- Vostok.Hosting/Setup/IVostokDynamicThreadPoolBuilder.cs
+++ Vostok.Hosting/Setup/IVostokDynamicThreadPoolBuilder.cs
@@ -1,10 +1,21 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Configuration.Abstractions;
+using Vostok.Hosting.Components.ThreadPool;
 
 namespace Vostok.Hosting.Setup
 {
     [PublicAPI]
     public interface IVostokDynamicThreadPoolBuilder
     {
+        IVostokDynamicThreadPoolBuilder Enable();
+
+        IVostokDynamicThreadPoolBuilder Disable();
+
+        IVostokDynamicThreadPoolBuilder SetCheckPeriod(TimeSpan checkPeriod);
+
+        IVostokDynamicThreadPoolBuilder SetThreadPoolProvider(Func<IConfigurationProvider, ThreadPoolSettings> provider);
         
+        IVostokDynamicThreadPoolBuilder CustomizeDynamicThreadPoolSettings([NotNull] Action<DynamicThreadPoolTrackerSettings> customization);
     }
 }
