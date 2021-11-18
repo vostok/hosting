6ae49c61e9b10fdfa2d0adc754a0a4e1f0047268
Ilya Tzoop <ilyatzoop@gmail.com>
2021-11-18 10:15:12 +0500

    Stub for observable propagation

---
+++ Vostok.Hosting/Helpers/PropagateObservable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Vostok.Commons.Helpers.Disposable;
+
+namespace Vostok.Hosting.Helpers
+{
+    internal class PropagateObservable<T> : IObservable<T>, IObserver<T>
+    {
+        private readonly object guard = new();
+        private volatile IDisposable observableSubscription;
+        private volatile IObservable<T> baseObservable;
+        private readonly List<IObserver<T>> observers = new();
+
+        public void SetBaseObservable(IObservable<T> observable)
+        {
+            lock (guard)
+            {
+                observableSubscription?.Dispose();
+                baseObservable = observable;
+                observableSubscription = baseObservable.Subscribe(this);
+            }
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            lock (guard)
+            {
+                observers.Add(observer);
+            }
+
+            return new ActionDisposable(() =>
+            {
+                lock (guard)
+                {
+                    observers.Remove(observer);
+                }
+            });
+        }
+
+        public void OnCompleted()
+        {
+            lock (guard)
+            {
+                foreach (var observer in observers)
+                    observer.OnCompleted();
+            }
+        }
+
+        public void OnError(Exception error)
+        {
+            lock (guard)
+            {
+                foreach (var observer in observers)
+                    observer.OnError(error);
+            }
+        }
+
+        public void OnNext(T value)
+        {
+            lock (guard)
+            {
+                foreach (var observer in observers)
+                    observer.OnNext(value);
+            }
+        }
+    }
+}
