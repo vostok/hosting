5ededc7260873b5f10627b69d4ff94bbab836064
Ilya Shumilin <shumilin@skbkontur.ru>
2021-12-27 18:01:23 +0500

    Add hercules service discovery events sender builder

---
+++ Vostok.Hosting/Setup/IVostokHerculesServiceDiscoveryEventsSenderBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Setup
+{
+    [PublicAPI]
+    public interface IVostokHerculesServiceDiscoveryEventsSenderBuilder
+    {
+        bool IsEnabled { get; }
+
+        IVostokHerculesServiceDiscoveryEventsSenderBuilder Enable();
+        IVostokHerculesServiceDiscoveryEventsSenderBuilder Disable();
+
+        IVostokHerculesServiceDiscoveryEventsSenderBuilder SetStream([NotNull] string stream);
+        IVostokHerculesServiceDiscoveryEventsSenderBuilder SetApiKeyProvider([NotNull] Func<string> apiKeyProvider);
+    }
+}
