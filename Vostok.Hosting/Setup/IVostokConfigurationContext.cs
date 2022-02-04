af3099ec3e8e54e81edf3d43aa39f91087c086c8
kungurtsev <kungurtsev@skbkontur.ru>
2022-02-04 14:44:01 +0500

    add MergedConfigurationSource to HostExtensions

--- Vostok.Hosting/Setup/IVostokConfigurationContext.cs
+++ Vostok.Hosting/Setup/IVostokConfigurationContext.cs
@@ -16,7 +16,11 @@ namespace Vostok.Hosting.Setup
         [NotNull]
         IConfigurationSource SecretConfigurationSource { get; }
         
-        /// Combined <see cref="ConfigurationSource"/> and <see cref="SecretConfigurationSource"/>.
+        /// <summary>
+        /// <para>A source of raw secret configuration parameters provided by the host system.</para>
+        /// <para>Consists of combined sources of <see cref="ConfigurationSource"/> and <see cref="SecretConfigurationSource"/>.</para>
+        /// <para>Use it in conjunction with <see cref="ConfigurationProvider"/>.</para>
+        /// </summary>
         [NotNull]
         IConfigurationSource MergedConfigurationSource { get; }
 
