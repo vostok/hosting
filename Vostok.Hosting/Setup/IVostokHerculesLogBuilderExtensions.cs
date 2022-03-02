913b7c29dc8385b3c81126cb7d57e1db05c2c15f
Ilya Tzoop <ilyatzoop@gmail.com>
2022-03-02 11:20:54 +0500

    Append hostname extension

--- Vostok.Hosting/Setup/IVostokHerculesLogBuilderExtensions.cs
+++ Vostok.Hosting/Setup/IVostokHerculesLogBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using Vostok.Commons.Environment;
 using Vostok.Logging.Abstractions;
 using Vostok.Logging.Hercules;
 
@@ -7,6 +8,9 @@ namespace Vostok.Hosting.Setup
     [PublicAPI]
     public static class IVostokHerculesLogBuilderExtensions
     {
+        public static IVostokHerculesLogBuilder SetHostname(this IVostokHerculesLogBuilder builder, [CanBeNull] string hostName = null) =>
+            builder.CustomizeLog(log => log.WithProperty("hostName", hostName ?? EnvironmentInfo.Host));
+
         public static IVostokHerculesLogBuilder SetElkIndex([NotNull] this IVostokHerculesLogBuilder builder, string elkIndex) =>
             builder.CustomizeLog(log => log.WithProperty(WellKnownHerculesLogProperties.ElkIndex, elkIndex));
 
