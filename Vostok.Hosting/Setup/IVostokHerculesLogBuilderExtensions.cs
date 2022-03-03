58de1234f771b96093385c2d18681edd9d148609
Ilya Tzoop <ilyatzoop@gmail.com>
2022-03-03 11:34:27 +0500

    Move SetHostName to IVostokCompositeLogBuilder

--- Vostok.Hosting/Setup/IVostokHerculesLogBuilderExtensions.cs
+++ Vostok.Hosting/Setup/IVostokHerculesLogBuilderExtensions.cs
@@ -1,5 +1,4 @@
 using JetBrains.Annotations;
-using Vostok.Commons.Environment;
 using Vostok.Logging.Abstractions;
 using Vostok.Logging.Hercules;
 
@@ -8,9 +7,6 @@ namespace Vostok.Hosting.Setup
     [PublicAPI]
     public static class IVostokHerculesLogBuilderExtensions
     {
-        public static IVostokHerculesLogBuilder SetHostname(this IVostokHerculesLogBuilder builder, [CanBeNull] string hostName = null) =>
-            builder.CustomizeLog(log => log.WithProperty("hostName", hostName ?? EnvironmentInfo.Host));
-
         public static IVostokHerculesLogBuilder SetElkIndex([NotNull] this IVostokHerculesLogBuilder builder, string elkIndex) =>
             builder.CustomizeLog(log => log.WithProperty(WellKnownHerculesLogProperties.ElkIndex, elkIndex));
 
