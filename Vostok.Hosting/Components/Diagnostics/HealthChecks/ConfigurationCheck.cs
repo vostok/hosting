131dedb9ab921c67484268682132e30ae4ebc581
kungurtsev <kungurtsev@skbkontur.ru>
2022-06-20 17:46:58 +0500

    add configuration health check

---
+++ Vostok.Hosting/Components/Diagnostics/HealthChecks/ConfigurationCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Vostok.Configuration;
+using Vostok.Hosting.Abstractions.Diagnostics;
+
+namespace Vostok.Hosting.Components.Diagnostics.HealthChecks;
+
+[PublicAPI]
+public class ConfigurationCheck : IHealthCheck
+{
+    private readonly ConfigurationProvider configurationProvider;
+
+    public ConfigurationCheck(ConfigurationProvider configurationProvider) =>
+        this.configurationProvider = configurationProvider;
+
+    public Task<HealthCheckResult> CheckAsync(CancellationToken cancellationToken)
+    {
+        var reason = configurationProvider.GetHealthStatus();
+
+        var result = new HealthCheckResult(reason == null ? HealthStatus.Healthy : HealthStatus.Degraded, reason);
+
+        return Task.FromResult(result);
+    }
+}
