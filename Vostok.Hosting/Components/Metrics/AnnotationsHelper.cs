efcf3941f20d5753c5ae986bb8369d7bcc7034e1
ingevarr <mynik04@mail.ru>
2022-11-24 12:23:08 +0500

    Add way to enrich lifecycle annotation tags with new host extension

--- Vostok.Hosting/Components/Metrics/AnnotationsHelper.cs
+++ Vostok.Hosting/Components/Metrics/AnnotationsHelper.cs
@@ -1,4 +1,6 @@
-﻿using Vostok.Commons.Environment;
+﻿using System.Linq;
+using JetBrains.Annotations;
+using Vostok.Commons.Environment;
 using Vostok.Hosting.Abstractions;
 using Vostok.Metrics;
 
@@ -9,19 +11,25 @@ namespace Vostok.Hosting.Components.Metrics
         private const string HostTag = "host";
         private const string EventTypeTag = "vostokEvent";
 
-        public static void ReportLaunching(IVostokApplicationIdentity identity, IMetricContext context)
+        public static void ReportLaunching(IVostokApplicationIdentity identity, IMetricContext context, [CanBeNull] LifecycleAnnotationsAdditionalTags additionalTags)
             => context.SendAnnotation($"Instance {identity.Instance} is launching (just started) on host {EnvironmentInfo.Host}.",
-                (HostTag, EnvironmentInfo.Host.ToLowerInvariant()),
-                (EventTypeTag, "Launching"));
+                ConstructTags("Launching", additionalTags));
 
-        public static void ReportInitialized(IVostokApplicationIdentity identity, IMetricContext context)
+        public static void ReportInitialized(IVostokApplicationIdentity identity, IMetricContext context, [CanBeNull] LifecycleAnnotationsAdditionalTags additionalTags)
             => context.SendAnnotation($"Instance {identity.Instance} has initialized (running now) on host {EnvironmentInfo.Host}.",
-                (HostTag, EnvironmentInfo.Host.ToLowerInvariant()),
-                (EventTypeTag, "Initialized"));
+                ConstructTags("Initialized", additionalTags));
 
-        public static void ReportStopping(IVostokApplicationIdentity identity, IMetricContext context)
+        public static void ReportStopping(IVostokApplicationIdentity identity, IMetricContext context, [CanBeNull] LifecycleAnnotationsAdditionalTags additionalTags)
             => context.SendAnnotation($"Instance {identity.Instance} is stopping (graceful shutdown triggered) on host {EnvironmentInfo.Host}.",
-                (HostTag, EnvironmentInfo.Host.ToLowerInvariant()),
-                (EventTypeTag, "Stopping"));
+                ConstructTags("Stopping", additionalTags));
+
+        private static (string, string)[] ConstructTags(string eventType, [CanBeNull] LifecycleAnnotationsAdditionalTags additionalTags)
+        {
+            var tags = additionalTags?.Tags ?? Enumerable.Empty<(string, string)>();
+            return tags
+                .Append((HostTag, EnvironmentInfo.Host.ToLowerInvariant()))
+                .Append((EventTypeTag, eventType))
+                .ToArray();
+        }
     }
 }
