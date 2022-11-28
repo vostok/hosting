bb6077b16401a5972f9466a1893ee1cee0158fae
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2022-11-28 15:25:27 +0500

    Add test

--- Vostok.Hosting.Tests/Components/Metrics/TagsEnrichingAnnotationEventSender_Tests.cs
+++ Vostok.Hosting.Tests/Components/Metrics/TagsEnrichingAnnotationEventSender_Tests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
+using Vostok.Hosting.Abstractions;
 using Vostok.Hosting.Components.Metrics;
 using Vostok.Metrics;
 using Vostok.Metrics.Models;
@@ -10,20 +11,39 @@ namespace Vostok.Hosting.Tests.Components.Metrics;
 
 internal sealed class TagsEnrichingAnnotationEventSender_Tests
 {
-    [Test]
-    public void Should_enrich_annotation_tags()
+    private IAnnotationEventSender baseSender;
+    private AnnotationEvent enrichedEvent;
+
+    [SetUp]
+    public void SetUp()
     {
-        var baseSender = Substitute.For<IAnnotationEventSender>();
-
-        AnnotationEvent enrichedEvent = null;
+        baseSender = Substitute.For<IAnnotationEventSender>();
         baseSender.When(s => s.Send(Arg.Any<AnnotationEvent>())).Do(info => enrichedEvent = info.Arg<AnnotationEvent>());
+    }
+    
+    [Test]
+    public void Should_enrich_annotation_tags_when_has_instance_tag()
+    {
+        var instanceTag = new MetricTag(WellKnownApplicationIdentityProperties.Instance, "instance");
+        var additionalTag = new MetricTag("initialTagKey", "initialTagValue");
 
-        var initialTag = new MetricTag("initialTagKey", "initialTagValue");
         var newTag = new MetricTag("key", "value");
-        
         var sender = new TagsEnrichingInstanceAnnotationEventSender(baseSender, new MetricTags(newTag));
-        sender.Send(new AnnotationEvent(DateTimeOffset.Now, new MetricTags(initialTag), "Annotation"));
 
-        enrichedEvent.Tags.Should().BeEquivalentTo(initialTag, newTag);
+        sender.Send(new AnnotationEvent(DateTimeOffset.Now, new MetricTags(instanceTag, additionalTag), "Annotation"));
+
+        enrichedEvent.Tags.Should().BeEquivalentTo(instanceTag, additionalTag, newTag);
+    }
+    
+    [Test]
+    public void Should_not_enrich_annotation_tags_when_has_not_instance_tag()
+    {
+        var additionalTag = new MetricTag("initialTagKey", "initialTagValue");
+        var newTag = new MetricTag("key", "value");
+
+        var sender = new TagsEnrichingInstanceAnnotationEventSender(baseSender, new MetricTags(newTag));
+        sender.Send(new AnnotationEvent(DateTimeOffset.Now, new MetricTags(additionalTag), "Annotation"));
+
+        enrichedEvent.Tags.Should().BeEquivalentTo(additionalTag);
     }
 }
